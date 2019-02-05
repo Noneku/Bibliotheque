4da1a8cb1d3fdab8d8748d701101e10783db985e
Vincent <vincentrichardeau@laposte.net>
2019-02-05 15:34:16 +0100

    add single view + single informations

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Livre;
 class LivreController extends AbstractController
 {
     /**
@@ -14,6 +14,18 @@ class LivreController extends AbstractController
     {
         return $this->render('livre/index.html.twig', [
             'controller_name' => 'LivreController',
+        ]);
+    }
+
+    /**
+     * @Route("/livre{id}", name="app_getLivre{id}")
+     */
+    public function getLivre($id)
+    {   
+        $repository = $this->getDoctrine()->getRepository(Livre::class);
+        $Livre = $repository->findOneBy(array('id' => $id));   
+        return $this->render('livre/singleLivre.html.twig', [
+            'id' => $id, 'Livre' => $Livre
         ]);
     }
 }
