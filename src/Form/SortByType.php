7a646a091cd97f3f9dbcc2beb962455519fe98f9
Vincent <vincentrichardeau@laposte.net>
2019-02-11 16:11:02 +0100

    modify sort

--- src/Form/SortByType.php
+++ src/Form/SortByType.php
@@ -29,7 +29,6 @@ class SortByType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Category::class,
         ]);
     }
 }
