118400a5f0451b40cda40c035c8572d14e63d807
Vincent <vincentrichardeau@laposte.net>
2019-02-12 09:37:36 +0100

    create method for emprunteur

--- src/Entity/Livre.php
+++ src/Entity/Livre.php
@@ -120,6 +120,12 @@ class Livre
     public function setEmprunteur(?Emprunteur $emprunteur): self
     {
         $this->emprunteur = $emprunteur;
+        if ($emprunteur) {
+            $this->setStatus(0);
+        }
+        else {
+            $this->setStatus(1);
+        }
         return $this;
     }
 
