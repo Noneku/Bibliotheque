cf3fbe24fec97b7c85a1b2747fd2a63671fdcb1c
Vincent <vincentrichardeau@laposte.net>
2019-02-12 15:42:45 +0100

    add fixtures for users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,13 +12,7 @@ class AppFixtures extends Fixture
     {
         // create 20 products! Bam!
         for ($i = 0; $i < 20; $i++) {
-            $livre = new Livre();
-            $livre->setTitre('livre '.$i);
-            $livre->setAuteur('livre '.$i);
-            $livre->setResume('livre '.$i);
-            $livre->setStatus(1);
-            $livre->setDateParution();
-            $manager->persist($livre);
+
         }
 
         $manager->flush();
---
+++ src/DataFixtures/Userfixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+class Userfixtures extends Fixture
+{
+    private $passwordEncoder;
+
+         public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+         {
+             $this->passwordEncoder = $passwordEncoder;
+         }
+    
+    public function load(ObjectManager $manager)
+    {
+            for ($i=0; $i < 10; $i++) {
+            $user = new User();     
+            $user->setUsername("vincent$i");
+            $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            "password$i"
+            ));
+            $user->setRoles(["ROLE_ADMIN"]);
+            $manager->persist($user);
+         }
+        $manager->flush();
+
+    }
+}
